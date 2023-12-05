188ea98d82757b0fc173bc50e24676ee5e07b34b
Beat Temperli <beat@temper.li>
2023-12-05 20:03:22 +0100

    add descriptive comments

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -6,10 +6,10 @@ public final class Constants {
         public static final double normalSpeed = 1;
         public static final double encoderToMetersConversion = -22.14274406;
 
-        public static final int motorL_ID = 10;
-        public static final int motorL2_ID = 11;
-        public static final int motorR_ID = 12;
-        public static final int motorR2_ID = 13;
+        public static final int motorL_ID = 10; // master
+        public static final int motorL2_ID = 11; // follower
+        public static final int motorR_ID = 12; // master
+        public static final int motorR2_ID = 13; // follower
     }
 
     public static class Controller {
